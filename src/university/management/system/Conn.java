1ed0ebb31309cb7691b3fc995631ad0f42b7c274
AmanSwarnim07 <amanswarnim07@gmail.com>
2024-12-30 00:34:04 +0530

    University Management System

---
+++ src/university/management/system/Conn.java
@@ -0,0 +1,22 @@
+
+package university.management.system;
+
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+
+    Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///universitymanagementsystem", "root", "Jiya2005");
+            s = c.createStatement();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
